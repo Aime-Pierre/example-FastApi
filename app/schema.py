550e696abbd778a47e225f98c60a2948a73eed57
Aime-Pierre <blessedchildap@gmail.com>
2025-05-07 19:02:45 +0200

    update user response schema

--- app/schema.py
+++ app/schema.py
@@ -1,10 +1,15 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, field_validator
 from typing import Optional
+from datetime import datetime
 
 class UserResponse(BaseModel):
     id: int
     email: str
-    created_at: str
+    created_at: datetime
+    @field_validator('created_at')
+    def format_datetime(cls, v: datetime) -> str:
+        return v.isoformat()
+    
 
 class PostResponse(BaseModel):
     id: int
